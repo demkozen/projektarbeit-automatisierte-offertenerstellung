8e78404d03245f5248d864245672b88b22e9d6dc
unknown <adem.zenuni@stud.hslu.ch>
2024-11-09 15:17:51 +0100

    Auflistung der Labels im persönlichen GMAIL Account

--- main.py
+++ main.py
@@ -1,55 +1,55 @@
-import os.path
-
+import os
+import base64
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-# If modifying these scopes, delete the file token.json.
+# GMAIL Scope / Berechtigungsbereich
 SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
 
+def get_gmail_service():
+    #Authentifiziert den Benutzer und gibt den Gmail-Service zurück.
+    creds = None
+
+    # Prüfung ob token.json existiert.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+
+    # Wenn keine Credentials vorhanden, dann den Authentifizierungsprozess initialisieren.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+    # Erstellen der Credentials und return der GmailServices
+    return build("gmail", "v1", credentials=creds)
+
+
+
+
+
+def list_labels(service):
+    #Auflistung aller Labels für Testzwecke
+    try:
+        results = service.users().labels().list(userId="me").execute()
+        labels = results.get("labels", [])
+        if not labels:
+            print("No labels found.")
+            return
+        print("Labels:")
+        for label in labels:
+            print(label["name"])
+    except HttpError as error:
+        print(f"An error occurred: {error}")
 
 def main():
-  """Shows basic usage of the Gmail API.
-  Lists the user's Gmail labels.
-  """
-  creds = None
-  # The file token.json stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
-  if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-  # If there are no (valid) credentials available, let the user log in.
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", SCOPES
-      )
-      creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open("token.json", "w") as token:
-      token.write(creds.to_json())
-
-  try:
-    # Call the Gmail API
-    service = build("gmail", "v1", credentials=creds)
-    results = service.users().labels().list(userId="me").execute()
-    labels = results.get("labels", [])
-
-    if not labels:
-      print("No labels found.")
-      return
-    print("Labels:")
-    for label in labels:
-      print(label["name"])
-
-  except HttpError as error:
-    # TODO(developer) - Handle errors from gmail API.
-    print(f"An error occurred: {error}")
-
+    service = get_gmail_service()
+    list_labels(service)  # Labels abrufen
 
 if __name__ == "__main__":
-  main()
+    main()
